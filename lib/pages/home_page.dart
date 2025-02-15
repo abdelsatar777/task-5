569fc038966d855b05fe06a456a80b92cc9698f5
Ahmed Nabil <153556684+AhmedNSharaf@users.noreply.github.com>
2025-02-15 11:37:54 +0200

    ahmeds work

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_5/widgets/custom_card_1.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,41 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 165, 187, 210),
+      appBar: AppBar(
+        title: const Text(
+          'News App',
+          style: TextStyle(color: Colors.black, fontSize: 30),
+        ),
+        backgroundColor: const Color.fromARGB(255, 4, 125, 117),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 166,
+                width: double.infinity,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (BuildContext context, int index) {
+                    return CustomCard1();
+                  },
+                ),
+              ),
+              Text('General News',
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black)),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/widgets/custom_card_1.dart
+++ lib/widgets/custom_card_1.dart
@@ -5,6 +5,35 @@ class CustomCard1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.only(right: 10.0),
+      child: Container(
+        width: 150,
+        height: 150,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(200),
+        ),
+        child: Stack(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(200),
+              child: Image.asset(
+                'assets/WhatsApp Image 2024-07-03 at 19.56.48_ae64950a.jpg',
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 70.0, left: 50),
+              child: Text(
+                'Health',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }

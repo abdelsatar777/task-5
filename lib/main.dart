52aa2a723e4ebe31c7d88825dacd62f6aad19783
abdelsatar777 <mohammedabdelsatar2006@gmail.com>
2025-02-15 11:36:34 +0200

    Build Card 2 Widgets 'Abdelsatar'

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class Task5 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }

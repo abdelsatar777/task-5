96de4c5bac1dab5e98f0101e9d606b99fb06e35c
abdelsatar777 <mohammedabdelsatar2006@gmail.com>
2025-02-15 10:59:42 +0200

    Creaate 2 Card Widgets 'Abdelsatar'

---
+++ lib/widgets/custom_card_1.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomCard1 extends StatelessWidget {
+  const CustomCard1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/widgets/custom_card_2.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomCard2 extends StatelessWidget {
+  const CustomCard2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
